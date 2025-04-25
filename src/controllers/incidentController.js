37a40fa737c7ea5dc35a1cc21dcb9351454334f5
Praveen Kumar <115071711+Praveenkumar07007@users.noreply.github.com>
2025-04-25 22:24:24 +0530

    Implement incident creation functionality and update routes to include createIncident and tested

--- src/controllers/incidentController.js
+++ src/controllers/incidentController.js
@@ -8,3 +8,24 @@ export const getAllIncidents = async (req, res) => {
         res.status(500).json({ message: 'Error fetching incidents', error: error.message });
     }
 };
+
+
+export const createIncident = async (req, res) => {
+    const { title, description, severity } = req.body;
+    if (!title || !description || !severity) {
+        return res.status(400).json({ message: 'Missing required fields: title, description, severity' });
+    }
+    if (!['Low', 'Medium', 'High'].includes(severity)) {
+        return res.status(400).json({ message: 'Invalid severity level. Must be (low, Medium, high)' });
+    }
+    try {
+        const newIncident = new Incident({ title, description, severity });
+        const savedIncident = await newIncident.save();
+        res.status(201).json(savedIncident);
+    } catch (error) {
+        if (error.name === 'ValidationError') {
+            return res.status(400).json({ message: 'Validation Error', errors: error.errors });
+        }
+        res.status(500).json({ message: 'Error creating incident', error: error.message });
+    }
+};
--- src/routes/incidentRoutes.js
+++ src/routes/incidentRoutes.js
@@ -1,7 +1,7 @@
 import express from 'express';
-import { getAllIncidents } from '../controllers/incidentController.js';
+import { getAllIncidents, createIncident } from '../controllers/incidentController.js';
 const router = express.Router();
 
 router.get('/', getAllIncidents);
-
+router.post('/', createIncident);
 export default router;
